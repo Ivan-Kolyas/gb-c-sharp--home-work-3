6112d06fcf606df05d72561c1430cfc1262fc349
Ivan-Kolyas <ivandaodao@gmail.com>
2022-12-23 13:46:18 +0300

    dz 3 c sharp

---
+++ Program.cs
@@ -0,0 +1,89 @@
+﻿
+//Задача 19
+
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+
+
+//  int num, k, sum = 0, temp;
+        
+        
+//         Console.WriteLine("\n   Палиндром?  ");
+//         Console.Write("\n Enter a number: ");
+//         num = Convert.ToInt32(Console.ReadLine());
+//         temp = num;
+//         while (Convert.ToBoolean(num))
+//         {
+//             k = num % 10;  
+//             num = num / 10; 
+//             sum = sum * 10 + k; 
+//         }
+//         Console.WriteLine("\n The Reversed Number is: {0} \n", sum);
+//         if (temp == sum) 
+//         {
+//             Console.WriteLine("\n Число - палиндром \n\n");
+//         }
+//         else
+//         {
+//             Console.WriteLine("\n Число - не палиндром \n\n");
+//         }
+//         Console.ReadLine();
+
+
+
+
+
+
+
+
+
+// Задача 21
+
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+
+
+
+
+// double GetLens(int x1,int y1,int z1,int x2,int y2,int z2)
+//  {
+//      double len = Math.Sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2) + (z1 - z2) * (z1 - z2));
+//      return len;
+//  }
+//   Console.WriteLine(GetLens(3,6,8,2,1,-7));
+
+
+
+
+
+
+
+
+
+
+
+
+// Задача 23
+
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+
+// void Cub (int n)
+// {
+//   for (int i = 1; i <= n; i++)
+//    {
+//      Console.WriteLine($"{i} -> {i*i*i}");
+//   }
+
+// }
+// Console.Write ("Введите число n = ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// Cub(n);
+
+
+
+
+
+
+
+
